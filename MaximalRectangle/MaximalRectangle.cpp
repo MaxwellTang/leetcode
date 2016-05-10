1accfe548748dc99fef4c36887506744b5621c45
MaxwellTang <tjx19920310@163.com>
2016-05-10 14:45:41 +0800

    add&push posts

---
+++ MaximalRectangle/MaximalRectangle.cpp
@@ -0,0 +1,18 @@
+/*
+Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing all ones and return its area.
+
+ Subscribe to see which companies asked this question
+*/
+class Solution {
+
+    public:
+
+        int maximalRectangle(vector<vector<char>>& matrix) {
+
+
+
+
+        }
+
+
+};
